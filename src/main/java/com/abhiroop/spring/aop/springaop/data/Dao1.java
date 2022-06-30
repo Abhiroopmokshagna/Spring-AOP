65db8cba23d2ec10e16f1a9f2eba387074304393
AB59574 <AB59574@POOLW42NLPC1094.mshome.net>
2022-06-30 11:16:47 +0530

    initial commit

---
+++ src/main/java/com/abhiroop/spring/aop/springaop/data/Dao1.java
@@ -0,0 +1,10 @@
+package com.abhiroop.spring.aop.springaop.data;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Dao1 {
+	public String retrieveSomething() {
+		return "Dao1";
+	}
+}
